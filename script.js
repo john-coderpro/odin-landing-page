748b80a71a9ecd47c6ec09a95d381903e3303a41
john-coderpro <johnchrisdevelops@gmail.com>
2022-07-29 06:23:01 -0400

    Fix the typo made on the root class

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // Nothing here for now but maybe there'll be something soon
+function pascalTriangle(row, col) {
+    if (col == 0) {
+        return 0;
+    } else if (row == 0) {
+        return 1;
+    } else {
+        return pascalTriangle(row - 1, col) + pascalTriangle(row - 1,
+            col - 1);
+    }
+}
